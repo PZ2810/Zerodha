3b0ef3f183e9ef317976e788bc9430a43597993d
Pranav Zhawar <pranavzhawar@gmail.com>
2026-03-07 02:24:08 +0530

    updated Support section

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -3,11 +3,11 @@ import React from "react";
 function Hero() {
   return (
     <section className="container-fluid" id="supportHero">
-      <div className="p-5 " id="supportWrapper">
+      <div className="p-3 " id="supportWrapper">
         <h4>Support Portal</h4>
         <a href="">Track Tickets</a>
       </div>
-      <div className="row p-5 m-3">
+      <div className="row p-3 m-3">
         <div className="col-6 p-3">
           <h1 className="fs-3">
             Search for an answer or browse help topics to create a ticket
